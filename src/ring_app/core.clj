134d513eaccce1b18703a08b2bb4e0a573a96f3c
Hoon Wee <mrgravity817@gmail.com>
2023-06-25 23:16:57 +0900

    feat: add tags that can be used in template

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -5,27 +5,7 @@
    [reitit.ring :as reitit]
    [ring.adapter.jetty :as jetty]
    [ring.middleware.reload :refer [wrap-reload]]
-   [ring.util.http-response :as response]
-   [selmer.parser :as selmer]
-   [selmer.filters :as filters]))
-
-;; Use selmer to create HTML template
-(selmer/render-file "hello.html" {:name "Hoon" :items (range 10)})
-;; We can also designate the dfeault resource path
-;; (selmer.parser/set-resource-path! "/var/html/templates/")
-(selmer/render
- "<p>Hello {{user.first}} {{user.last}}!</p>"
- {:user {:first "Hoon" :last "Wee"}})
-
-;; We can add filters, which preprocesses 
-;; the data before being rendered in HTML.
-(filters/add-filter! :empty? empty?)
-(selmer/render
- "{% if files|empty? %}no files{% else %}files{% endif %}"
- {:files []})
-
-(filters/add-filter! :foo (fn [x] [:safe (.toUpperCase x)]))
-(selmer/render "{{x|foo}}" {:x "<div>I'm safe</div>"})
+   [ring.util.http-response :as response]))
 
 (defn html-handler [request-map]
   (response/ok
---
+++ src/ring_app/templating.clj
@@ -0,0 +1,39 @@
+(ns ring-app.templating
+  (:require
+   [selmer.parser :as selmer]
+   [selmer.filters :as filters]))
+
+;; Use selmer to create HTML template
+(selmer/render-file "hello.html" {:name "Hoon" :items (range 10)})
+;; We can also designate the dfeault resource path
+;; (selmer.parser/set-resource-path! "/var/html/templates/")
+(selmer/render
+ "<p>Hello {{user.first}} {{user.last}}!</p>"
+ {:user {:first "Hoon" :last "Wee"}})
+
+;; We can add filters, which preprocesses 
+;; the data before being rendered in HTML.
+(filters/add-filter! :empty? empty?)
+(selmer/render
+ "{% if files|empty? %}no files{% else %}files{% endif %}"
+ {:files []})
+
+(filters/add-filter! :foo (fn [x] [:safe (.toUpperCase x)]))
+(selmer/render "{{x|foo}}" {:x "<div>I'm safe</div>"})
+
+;; Define custom tags 
+;; {% image "src" %}
+(selmer/add-tag!
+ :image
+ (fn [args context-map] (str "<img src=" (first args) "/>")))
+(selmer/render "{% image \"http://foo.com/logo.jpg\" %}" {})
+
+;; {% uppercase %}
+;;   some texts to be uppercase
+;; {% enduppercase %}
+(selmer/add-tag!
+ :uppercase
+ (fn [args context-map block]
+   (.toUpperCase (get-in block [:uppercase :content])))
+ :enduppercase)
+(selmer/render "{% uppercase %}foo {{bar}} baz{% enduppercase %}" {:bar "injected"})
