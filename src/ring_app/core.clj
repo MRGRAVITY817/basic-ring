1a9a07c955deb826d582e817894351f9b895b316
Hoon Wee <mrgravity817@gmail.com>
2023-06-25 16:59:52 +0900

    feat: add middlewares

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,16 +1,27 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            ;; We can bring pre-built middlewares
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler [request-map]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (str "<html><body> your IP is: "
+   :body (str "<html><body>Wow, your IP is: "
               (:remote-addr request-map)
               "</body></html>")})
 
+;; We can create our own middlewares
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
 (defn -main []
   (jetty/run-jetty
-   handler
+   (-> #'handler
+       wrap-nocache
+       wrap-reload)
    {:port 3333
     :join? false}))
 
