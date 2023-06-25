bfbb239772381931a4078dc05361ead345fb0280
Hoon Wee <mrgravity817@gmail.com>
2023-06-25 22:32:15 +0900

    feat: serve resources

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -40,13 +40,15 @@
 (def handler
   (reitit/ring-handler
    (reitit/router routes)
-   (reitit/create-default-handler
-    {:not-found
-     (c/constantly (response/not-found "404 - Page not found"))
-     :method-not-allowed
-     (c/constantly (response/method-not-allowed "405 - Not allowed"))
-     :not-acceptable
-     (c/constantly (response/not-acceptable "406 - Not acceptable"))})))
+   (reitit/routes
+    (reitit/create-resource-handler {:path "/"})
+    (reitit/create-default-handler
+     {:not-found
+      (c/constantly (response/not-found "404 - Page not found"))
+      :method-not-allowed
+      (c/constantly (response/method-not-allowed "405 - Not allowed"))
+      :not-acceptable
+      (c/constantly (response/not-acceptable "406 - Not acceptable"))}))))
 
 (defn -main []
   (jetty/run-jetty
